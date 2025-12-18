8f38dc6fd6a6a84d08778128f4d03418b19d57cb
Arpit Singh <arpitlibono57@gmail.com>
2025-12-18 18:25:44 +0530

    bug remove

--- src/components/Navbartop.jsx
+++ src/components/Navbartop.jsx
@@ -3,31 +3,31 @@ import { Contact, Download, Home, Presentation, User, Wrench } from 'lucide-reac
 
 const Navbartop = () => {
   return (
-    <div className="flex justify-center items-center p-2 mt-8">
-      <nav className="flex justify-evenly items-center h-[70px] w-[140vh] rounded-4xl bg-white/20 backdrop-blur-md border border-white/30 shadow-lg cursor-pointer">
+    <div className="flex justify-center items-center p-2 mt-8 text-sm :md:text-lg w-[70%] ">
+      <nav className="flex justify-evenly items-center h-[70px] w-full max-w-6xl  rounded-4xl bg-white/20 backdrop-blur-md border border-white/30 shadow-lg cursor-pointer">
 
-        <a href="#home" className="text-[17px] font-bold text-white flex items-center gap-1 hover:text-gray-200">
-          <Home size={20}/> Home
+        <a href="#home" className="text-md xl:text-xl font-bold text-white flex items-center gap-1 hover:text-gray-200">
+          <Home size={20}/><h1 className='hidden sm:block ' >Home</h1>
         </a>
 
-        <a href="#about" className="text-[17px] font-bold text-white flex items-center gap-1 hover:text-gray-200">
-          <User size={20}/> About
+        <a href="#about" className="text-md xl:text-xl font-bold text-white flex items-center gap-1 hover:text-gray-200">
+          <User size={20}/><h1 className='hidden sm:block ' >About</h1>
         </a>
           {/* skills */}
-        <a href="#Achieve" className="text-[17px] font-bold text-white flex items-center gap-1 hover:text-gray-200">
-          <Presentation size={20}/> Projects
+        <a href="#Achieve" className="text-md xl:text-xl font-bold text-white flex items-center gap-1 hover:text-gray-200">
+          <Presentation size={20}/><h1 className='hidden sm:block ' >Projects</h1>
         </a>
 
-        <a href="#projects" className="text-[17px] font-bold text-white flex items-center gap-1 hover:text-gray-200">
-          <Wrench size={20}/> Skills
+        <a href="#projects" className="text-md xl:text-xl font-bold text-white flex items-center gap-1 hover:text-gray-200">
+          <Wrench size={20}/><h1 className='hidden sm:block ' >Skills</h1>
         </a>
 
-        <a href="#resume" className="text-[17px] font-bold text-white flex items-center gap-1 hover:text-gray-200">
-          <Download size={20}/> Resume
+        <a href="#resume" className="text-md xl:text-xl font-bold text-white flex items-center gap-1 hover:text-gray-200">
+          <Download size={20}/><h1 className='hidden sm:block ' >Resume</h1>
         </a>
 
-        <a href="#contact" className="text-[17px] font-bold text-white flex items-center gap-1 hover:text-gray-200">
-          <Contact size={20}/> Contact
+        <a href="#contact" className="text-md xl:text-xl font-bold text-white flex items-center gap-1 hover:text-gray-200">
+          <Contact size={20}/><h1 className='hidden sm:block ' >Contact</h1>
         </a>
 
       </nav>
